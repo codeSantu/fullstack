c49fa098d408beee6b0040e70afcf62ea0370a5f
codeSantu <100938870+codeSantu@users.noreply.github.com>
2026-03-15 21:16:20 +0530

    fix: url

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -52,6 +52,7 @@ async function bootstrap() {
         'https://jmks.vercel.app',
         'http://localhost:3000',
         'http://localhost:3001',
+        'https://jmksangha.netlify.app'
     ];
 
     app.enableCors({
