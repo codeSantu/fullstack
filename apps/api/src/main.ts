c9b958596c94983d88b80e045bdf1bea768a17c9
codeSantu <100938870+codeSantu@users.noreply.github.com>
2026-03-16 07:57:24 +0530

    feat: Configure NestJS application bootstrap with global logging, security, validation, Swagger, and CORS.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -55,6 +55,7 @@ async function bootstrap() {
         'http://localhost:3001',
         'https://jmksangha.netlify.app',
     ];
+    //hi
 
     app.enableCors({
         origin: (origin, callback) => {
